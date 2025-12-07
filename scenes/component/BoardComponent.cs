eabe42e98e144977a30eeda34e0ef86d7bcc86f6
leoarmelin <leonardo99.armelin@gmail.com>
2025-12-07 19:01:17 -0300

    feat(board): add missing walls

--- scenes/component/BoardComponent.cs
+++ scenes/component/BoardComponent.cs
@@ -6,7 +6,6 @@ public partial class BoardComponent : Node2D
 {
 	public void AddPlayer(Player player)
 	{
-		player.GlobalPosition += new Vector2(16, 16);
 		AddChild(player);
 	}
 }
--- scenes/player/Player.cs
+++ scenes/player/Player.cs
@@ -9,6 +9,8 @@ public partial class Player : CharacterBody2D
 
   private bool isInMovement = false;
 
+  private static readonly Vector2 GRID = new Vector2(32, 32);
+
   public override void _Ready()
   {
     sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
@@ -18,10 +20,15 @@ public partial class Player : CharacterBody2D
   public override void _PhysicsProcess(double delta)
   {
     MoveAndSlide();
+
     if (Velocity == Vector2.Zero)
     {
       if (isInMovement)
       {
+        GD.Print(Position);
+        Position = Position.Snapped(GRID);
+        GD.Print(Position);
+
         EmitSignalOnStop();
         isInMovement = false;
       }
